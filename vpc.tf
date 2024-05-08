84554b2849e5ba014417a990580f02093090d7d5
ahmed6424 <ahmedssj6424@gmail.com>
2024-05-08 13:45:43 -0400

    create public route table

--- vpc.tf
+++ vpc.tf
@@ -40,3 +40,17 @@ resource "aws_internet_gateway" "ibm-igw" {
     Name = "ibm-internet-gateway"
   }
 }
+
+# Public Route Table
+resource "aws_route_table" "ibm-web-rt" {
+  vpc_id = aws_vpc.ibm-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/00"
+    gateway_id = aws_internet_gateway.ibm-igw.id
+  }
+
+  tags = {
+    Name = "ibm-web-route-table"
+  }
+}
