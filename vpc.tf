869ae2712ee1a9bc1e555167b430eab627ae48a4
ahmed6424 <ahmedssj6424@gmail.com>
2024-05-08 12:52:59 -0400

    create public subnet

--- vpc.tf
+++ vpc.tf
@@ -1,8 +1,21 @@
+# vpc
 resource "aws_vpc" "ibm-vpc" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
 
   tags = {
     Name = "ibm"
+  }
+}
+
+# public subnet
+resource "aws_subnet" "ibm-web-sn" {
+  vpc_id     = aws_vpc.ibm-vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1b"
+  map_public_ip_on_launch = "true"
+
+  tags = {
+    Name = "ibm-web-subnet"
   }
 }
