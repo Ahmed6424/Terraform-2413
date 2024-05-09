914b2b0b3c27f8a6243f51764ecf4f21a89dcb76
ahmed6424 <ahmedssj6424@gmail.com>
2024-05-09 17:10:38 -0400

    create web server

---
+++ ec2.tf
@@ -0,0 +1,12 @@
+# EC2 public sector
+resource "aws_instance" "ibm-web-server" {
+  ami           = "ami-0e001c9271cf7f3b9" # Ubuntu AMI & Amazon AMI ami-04ff98ccbfa41c9ad
+  instance_type = "t2.micro"
+  subnet_id = aws_subnet.ibm-web-sn.id
+  key_name = "7am"
+  security_groups = aws_security_group.ibm-web-sg.id
+
+  tags = {
+    Name = "ibm-web-server"
+  }
+}
