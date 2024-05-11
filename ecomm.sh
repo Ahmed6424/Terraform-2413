2ac5890b0580903dcc078bf58810b1f4475c40ab
ahmed6424 <ahmedssj6424@gmail.com>
2024-05-11 11:59:59 -0400

    ecomm app deployment

--- ecomm.sh
+++ ecomm.sh
@@ -2,4 +2,4 @@
 sudo apt update -y
 sudo apt install nginx -y
 sudo rm -rf /var/www/html/*
-sudo git clone https://github.com/Ahmed6424/ecomm.git /var/www/html/
+sudo git clone https://github.com/Ahmed6424/ecomm.git /var/www/html
