67db6be6c86e2968d6f6188841aa07a810635044
ahmed6424 <ahmedssj6424@gmail.com>
2024-05-11 12:00:39 -0400

    ecomm server setup

--- ec2.tf
+++ ec2.tf
@@ -1,12 +1,13 @@
 # EC2 public server
-resource "aws_instance" "ibm-web-server" {
+resource "aws_instance" "ecomm-web-server" {
   ami           = "ami-0e001c9271cf7f3b9" # Ubuntu AMI & Amazon AMI ami-04ff98ccbfa41c9ad
   instance_type = "t2.micro"
-  subnet_id = aws_subnet.ibm-web-sn.id
+  subnet_id = aws_subnet.ecomm-web-sn.id
   key_name = "7am"
-  vpc_security_group_ids = [aws_security_group.ibm-web-sg.id]
+  vpc_security_group_ids = [aws_security_group.ecomm-web-sg.id]
+  user_data = file("ecomm.sh")
 
   tags = {
-    Name = "ibm-web-server"
+    Name = "ecomm-web-server"
   }
 }
